9caa877fe826d0cd64a4e8c4d8aa69a3874b3c5f
Benjamin-Ohene-Asare <benjaminoheneasare65@gmail.com>
2025-01-02 13:24:55 -0800

    change

--- my_site/urls.py
+++ my_site/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('service', views.service, name='service'),
     path('contactUs', views.contactUs, name='contactUs'),
     path('booking', views.booking, name='booking'),
+    path('bookings', views.bookings, name='bookings'),
     path('signUp', views.signUp, name='signUp'),
     path('login', views.login, name='login'),
     path('termsAndCondition', views.termsAndCondition, name='termsAndCondition'),
