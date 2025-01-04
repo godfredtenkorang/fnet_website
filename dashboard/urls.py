8117f863e931d28c32eb7a7bd0d86b3af4a9373c
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-01-04 16:22:01 +0000

    made car and rental model

---
+++ dashboard/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('contact/', views.contact, name='contact'),
+    path('sendMessage/', views.sendMessage, name='sendMessage'),
+    path('bookings/', views.bookings, name='bookings'),
+]
