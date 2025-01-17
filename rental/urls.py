7b3474ff194d68b20c251de675b0276750ee5f0a
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-01-17 09:23:08 +0000

    changes

--- rental/urls.py
+++ rental/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('schedule-a-ride/', views.appointment, name='appointment'),
+    path('schedule-cars/', views.all_schedule_cars, name='all_schedule_cars'),
+    path('schedule-a-ride/<int:car_id>/book_now/', views.schedule_a_ride, name='appointment'),
     path('bookings/<slug:car_slug>/payment/', views.process_payment, name='process-payment'),
     path('sucess-page/', views.sucessPage, name='sucess-page'),
     path('unsucess-page/', views.unsucessPage, name='unsucess-page'),
