e6207e0b5a52524c08bd06d644748e9fd5c45804
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-01-29 23:51:18 +0000

    changes

--- agreements/forms.py
+++ agreements/forms.py
@@ -13,3 +13,5 @@ class DriverAgreementForm(forms.ModelForm):
     class Meta:
         model = DriverAgreement
         fields = ['driver_full_name', 'ghana_card_number', 'driving_license_number', 'license_expiration_date', 'guarantor_full_name', 'guarantor_ghana_card_number', 'date_of_birth', 'contact_number', 'residential_address', 'digital_address', 'relationship_to_driver', 'emergency_contact_number', 'representative_name']
+        
+        
